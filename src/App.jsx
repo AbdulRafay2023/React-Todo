da580b0eb2ceb15eaddb645b9ef6671fc827039c
Abdul Rafay <arafay6031@gmail.com>
2026-06-05 00:19:17 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./Components/Todo"
+
+const App = () => {
+  return (
+    <div className="app">
+     <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,10 @@
+const Todo = () => {
+  return (
+    <div className="todo"> 
+        <h1 className="bg-red-600">Todo List</h1>
+
+    </div>
+    )
+}
+
+export default Todo
