94c40e26919a4c32d873070496b5cacd91e147bc
Abdul Rafay <arafay6031@gmail.com>
2026-06-08 23:59:02 +0500

    Add delete functionality

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,43 +1,69 @@
-import checked from './Assets/checked.png'
-import unchecked from './Assets/unchecked.png'
-import cross from './Assets/cross.png'
+import checked from "./Assets/checked.png";
+import unchecked from "./Assets/unchecked.png";
+import cross from "./Assets/cross.png";
 
-const TodoItems = ({no,display,text,setTodos} )=>{
-   
-    const toggle = (no)=>{
-        let data = JSON.parse(localStorage.getItem("todos")) // all todos get from local storage and convert string to array/js object using json.parse
-        
-        for(let i=0; i<data.length; i++){
-            if(data[i].no === no){ // means if data no is equal with user click no 
-                 if(data[i].display === ""){
-                    data[i].display = "line-through" // for toggle line through
-            }else{
-                  data[i].display = "" // for toggle line through
-            }
-            break;
+const TodoItems = ({ no, display, text, setTodos }) => {
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos")); // all todos get from local storage and convert string to array/js object using json.parse
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        // means if data no is equal with user click no
+        if (data[i].display === "") {
+          data[i].display = "line-through"; // for toggle line through
+        } else {
+          data[i].display = ""; // for toggle line through
         }
-       }
-        setTodos(data);
+        break;
+      }
+    }
+    setTodos(data);
 
-        localStorage.setItem("todos", JSON.stringify(data));
-          
- 
-}
+    localStorage.setItem("todos", JSON.stringify(data));
+  };
 
-    return(
-        <div className="Todo-items flex justify-between items-center p-2 bg-gray-100 mb-2">
-           <div className="todo-items-container flex" onClick={()=>{toggle(no)}}  >
-             {display === "" ? <img className="w-[30px] " src={unchecked}/> : <img className="w-[30px] " src={checked}/>}
-            <span className={`pl-16px font-22px ${display === "line-through" ? "line-through" : ""}`}>{text}</span>
-            </div>
+  const deleteItem = (no) => {
+    console.log("delete", no);
+    let data = JSON.parse(localStorage.getItem("todos")) || [];
 
-            <div className="">
-                <img className="w-[10px] cursor-pointer " src={cross}/>
-            </div>            
-        </div>
-    )
-}
+    data = data.filter((item) => item.no !== no);
 
-export default TodoItems
+    setTodos(data);
 
+    localStorage.setItem("todos", JSON.stringify(data));
+  };
 
+  return (
+    <div className="Todo-items flex justify-between items-center p-2 bg-gray-100 mb-2">
+      <div
+        className="todo-items-container flex"
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img className="w-[30px] " src={unchecked} />
+        ) : (
+          <img className="w-[30px] " src={checked} />
+        )}
+        <span
+          className={`pl-16px font-22px ${display === "line-through" ? "line-through" : ""}`}
+        >
+          {text}
+        </span>
+      </div>
+
+      <div className="">
+        <img
+          className="w-[10px] cursor-pointer "
+          onClick={() => {
+            deleteItem(no);
+          }}
+          src={cross}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TodoItems;
